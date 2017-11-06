d90335638d80a9b715e806cd7fa193deb541c25d
Helli <hellauer@in.tum.de>
2017-11-06 19:40:19 +0100

    more slides and notes

--- paper.thy
+++ paper.thy
@@ -1,12 +1,22 @@
 (*<*)
 theory paper
-  imports "Regular-Sets.Regular_Set"
+  imports "Regular-Sets.Regexp_Method"
 begin
 (*>*)
 
 text\<open>Testing: @{thm Regular_Set.Arden}\<close>
 
+lemma "S O (S O S^* O R^* \<union> R^* ) \<subseteq> S O S^* O R^*"
+  by regexp
+
 section \<open>Usage of functional data structures\<close>(*Todo?*)
+
+lemma
+  assumes "\<And>A B. R A B \<Longrightarrow> [] \<in> A \<longleftrightarrow> [] \<in> B"
+  assumes "\<And>A B x. R A B \<Longrightarrow> R (Deriv x A) (Deriv x B)"
+  assumes "R A B"
+  shows "A = B"
+  using assms(1) assms(2) assms(3) language_coinduct by blast
 
 (*<*)
 end
