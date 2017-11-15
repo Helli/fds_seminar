5f60fc87912cf9c4e3e449e6b7221bf60a2c153c
Helli <hellauer@in.tum.de>
2017-11-15 18:17:50 +0100

    more for the paper draft

--- paper.thy
+++ paper.thy
@@ -14,18 +14,30 @@ alias closure = Equivalence_Checking.closure
 
 text\<open>The problem of equivalence between regular expressions is decidable. The simplest proof of this
 is also the only algorithm most textbooks teach: Convert both expressions into automata,
- determinize and minimize the result and check for equality (disregarding state identifiers). However, while
+ determinize and minimize the result and check for equality (disregarding state identifiers).
+ However, while
  this approach's correctness is obvious for CS graduates, verifying it is tedious, mostly because
- the formalization of automata theory requires a lot of notational overhead. Nipkow's paper @{cite
- "Krauss-Nipkow-JAR"} follows an alternative approach due to Brzozowski@{cite Brzozowski}. The
- development results in a ready-to-use proof method@{cite "Regular-Sets-AFP"} in Isabelle/HOL.
+ the formalization of automata theory requires a lot of notational overhead. Nipkow and Krauss@{cite
+ "Krauss-Nipkow-JAR"} stress this complexity in their introduction, to motivate why they follow an
+alternative approach due to Brzozowski@{cite Brzozowski}. The
+ development results in a ready-to-use proof method@{cite "Regular-Sets-AFP"} in Isabelle/HOL, replacing
+ the need for manual derivation of regular expression equivalences. The
+ avoiding of automata theory and Kleene-algebras leads to a much succincter proof than other verified
+equivalent checkers for regular expressions.
+
+What could possibly lead to such a large simplification? The authors must have developed their new
+concept  at some point after the \<^emph>\<open>Interactive Theorem Proving\<close> conference 2010 when Braibant and
+ Pous@{cite "Braibant2010"} presented their tactic for the theorem prover coq. While their acquired algorithm is quite
+ efficient and can handle arbitrary Kleene algebras, they need long and complex proofs for the verfication (~19000 lines).
 \<close>
 
 section\<open>Proof pearls\<close>
 text\<open>Not so often, experts publish articles so polished and clear, that they are called \<^emph>\<open>Proof
  pearl\<close>.
-The proof should be purposeful, and shorter than expected. The author references the work of Braibant and
- Pous@{cite Braibant2010}, whose verified equivalence checker is more efficient, but very complex in the derivation.
+The proof should be purposeful, and shorter than expected. The author references the work of
+ Braibant and 
+ Pous@{cite Braibant2010}, whose verified equivalence checker is more efficient, but very complex in
+ the derivation. 
  The paper's strength is that it comletely? ignores standard textbook-methods and finds inspiration
  instead in the Brzozowski's paper@{cite "Brzozowski"}, which fits perfectly into the world of
  interactive theorem proving due to its simplicity.
@@ -55,15 +67,104 @@ text\<open>The purpose of the article@{cite "Krauss-Nipkow-JAR"} is to provide a
 
 section\<open>Overview\<close>
 
-text\<open>Consider the following lemma, which is proven by word-length induction:
-@{thm [names_short] language_coinduct}
-Thus, we can obtain an equivalence proof by establishing such a relation \<open>\<sim>\<close> (called \<open>bisimulation\<close>).\<close>
+subsection\<open>What \<^emph>\<open>is\<close> in the procedure\<close>
+text\<open>As an introduction, Nipkow and Krauss reference the scientific work of Brzozowski@{cite
+ "Brzozowski"}.
+
+Remember the definition
+
+@{thm Deriv_def[no_vars]}
+
+What remains to do, is to define a computable operation @{const nderiv} on regular expressions which follows this
+ equation:
+
+@{thm lang_nderiv[no_vars]}
+<Beschreibung Rest vom Verfahren, todo>
+
+\<close>
 
 text\<open>Remember that relations are just sets of pairs. Our method will incrementally add language
   pairs (represented by increasingly complex regular expressions), maintaining the relation's
   bisimulation property. Once the examined regexes are added, equivalence for them is shown.\<close>
 
-section\<open>Nullability\<close>
+subsection\<open>What is \<^emph>\<open>not\<close> in the procedure\<close>
+
+subsection\<open>Notation\<close>
+
+text\<open>todo\<close>
+
+subsection\<open>language coinduction\<close>
+
+text\<open>Consider the following lemma, which is proven by word-length induction:
+
+@{thm [names_short] language_coinduct}
+
+<todo: bisimulation(unten) vs. ~>
+
+Thus, we can obtain an equivalence proof by establishing such a relation \<open>\<sim>\<close> (called \<open>bisimulation\<close>).
+
+But first of course, the authors prove it:
+
+@{theory_text \<open>
+
+proof -
+  define ps' where "ps' = insert (Zero, Zero) ps"
+  from bisim have bisim': "is_bisimulation as ps'"
+    by (auto simp: ps'_def is_bisimulation_def)
+  let ?R = "\<lambda>K L. (\<exists>(r,s)\<in>ps'. K = lang r \<and> L = lang s)"
+  show ?thesis
+  proof (rule language_coinduct[where R="?R"])
+    from \<open>(r, s) \<in> ps\<close> 
+    have "(r, s) \<in> ps'" by (auto simp: ps'_def)
+    thus "?R (lang r) (lang s)" by auto
+  next
+    fix K L assume "?R K L"
+    then obtain r s where rs: "(r, s) \<in> ps'"
+      and KL: "K = lang r" "L = lang s" by auto
+    with bisim' have "nullable r \<longleftrightarrow> nullable s"
+      by (auto simp: is_bisimulation_def)
+    thus "[] \<in> K \<longleftrightarrow> [] \<in> L" by (auto simp: nullable_iff KL)
+    fix a
+    show "?R (Deriv a K) (Deriv a L)"
+    proof cases
+      assume "a \<in> set as"
+      with rs bisim'
+      have "(nderiv a r, nderiv a s) \<in> ps'"
+        by (auto simp: is_bisimulation_def)
+      thus ?thesis by (force simp: KL lang_nderiv)
+    next
+      assume "a \<notin> set as"
+      with bisim' rs
+      have "a \<notin> atoms r" "a \<notin> atoms s" by (auto simp: is_bisimulation_def)
+      then have "nderiv a r = Zero" "nderiv a s = Zero"
+        by (auto intro: deriv_no_occurrence)
+      then have "Deriv a K = lang Zero" 
+        "Deriv a L = lang Zero" 
+        unfolding KL lang_nderiv[symmetric] by auto
+      thus ?thesis by (auto simp: ps'_def)
+    qed
+  qed  
+qed
+
+\<close>}\<close>
+
+section\<open>Regular Expressions\<close>
+subsection\<open>Notation\<close>
+
+text\<open>For regular expressions, we have the identifiers
+
+@{const Zero} for the regex with @{thm Regular_Exp.lang.simps(1)} and
+
+@{const One} for the regex with @{thm Regular_Exp.lang.simps(2)}
+
+referencing their properties in the corresponding Kleene algebras.
+Other than that, special syntax is completely avoided: All connectives are represented
+ with standard constructors (atoms need @{const Atom}, for instance).
+
+Words use standard list syntax.
+\<close>
+
+subsection\<open>Nullability\<close>
 
 thm nullable_iff
 text \<open>@{thm nullable.simps}\<close>
